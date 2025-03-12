ba7e262d1afabebe79fd7d0671d27412d07f57a0
abdillahsulthan <abdillahsulthan12@gmail.com>
2025-03-12 21:04:23 +0700

    Initial Commit

---
+++ TMSApp/TMSAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TMSAppApp.swift
+//  TMSApp
+//
+//  Created by Abdillah Sulthan Naufal Panggabean on 12/03/25.
+//
+
+import SwiftUI
+
+@main
+struct TMSAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
